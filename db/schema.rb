e069f9db1ae6c7b145439a27d8390664189ed2b7
Sebastián M <smunozmo@gmail.com>
2021-12-17 20:41:16 -0500

    Add App component.

--- db/schema.rb
+++ db/schema.rb
@@ -14,5 +14,11 @@ ActiveRecord::Schema.define(version: 2021_11_30_043255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  
+
+  create_table "greetings", force: :cascade do |t|
+    t.string "greeting"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 end
